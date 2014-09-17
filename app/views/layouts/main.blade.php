3bb0de113b3ebabbec2b4cffb4a5067410e84abd
Manthan787 <manthant15@gmail.com>
2014-09-17 22:17:20 +0530

    Y

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
         <title>@yield('title') | AstitvaStore</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="description" content="AstitvaStore is an Ecommerce website created to help people follow their hearts.">
 
         <!-- Bootstrap -->
         {{ HTML::style('bootstrap/css/bootstrap.css') }}
@@ -15,6 +16,17 @@
         <!--Jquery Init -->
         {{ HTML::script('js/jquery.js') }}
         {{ HTML::script('bootstrap/js/bootstrap.min.js') }}
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54904435-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
         
     </head>
 
@@ -742,16 +754,6 @@ Copyright Information
 {{ HTML::script('js/jquery-ui-custom.min.js') }}
 {{ HTML::script('js/jquery.cookie.js') }} 
 {{ HTML::script('js/script.js') }}
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-54904435-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 
 </body>
 </html>
