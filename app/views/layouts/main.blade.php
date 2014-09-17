ca0c3c70fba73a030d5ab45e0de8524ab7f48ec1
Manthan787 <manthant15@gmail.com>
2014-09-17 22:13:44 +0530

    X

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -742,6 +742,16 @@ Copyright Information
 {{ HTML::script('js/jquery-ui-custom.min.js') }}
 {{ HTML::script('js/jquery.cookie.js') }} 
 {{ HTML::script('js/script.js') }}
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54904435-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 </body>
 </html>
